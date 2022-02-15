f42c0408772d805d5abc17191eb5a7566f5b0b3b
giridhar30 <giriit30@gmail.com>
2022-02-15 09:01:06 +0530

    password encryption enabled

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,34 @@
+const bcryptjs = require("bcryptjs");
 const express = require("express");
 const User = require("../models/User");
 const { validateRegistration } = require("../validation");
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
+  // validating the user
   const { error } = validateRegistration(req.body);
   if (error) {
     return res.status(400).json({ error: error.details[0].message });
   }
 
+  // checking if user already exists
+  const userExists = await User.findOne({ email: req.body.email });
+  if (userExists) return res.status(400).json({ error: "User already exists" });
+
+  // encrypting the password
+  const salt = await bcryptjs.genSalt(10);
+  const hashedPassword = await bcryptjs.hash(req.body.password, salt);
+
   const user = new User({
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   });
 
   try {
     const savedUser = await user.save();
-    res.json(savedUser);
+    res.json({ user: savedUser._id });
   } catch (err) {
-    res.status(400).send(`${err} is the error`);
+    res.status(400).json({ error: `${err} is the error` });
   }
 });
 
